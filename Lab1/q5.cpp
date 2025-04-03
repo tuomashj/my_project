f7b8e2f270304dc69bd03c8915a2b8665f318fe9
Tuomas Jama <tuomas.jama@metropolia.fi>
2025-04-04 00:06:45 +0300

    question 5

---
+++ Lab1/q5.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int x; 
+    cout << "Gimme numba: ";
+    cin >> x;
+    string numbers = to_string(x);
+    int sum = 0;
+    for (int i = 0; i < numbers.length(); i++) {
+        int n = numbers[i]-48;
+        cout << n << "\n";
+        sum+=n;
+
+    }
+    cout << sum;
+    return 0;
+}
